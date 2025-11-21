4d3fb17e56a03d7fab73b5be9d7906610d75a25f
Joydeep Saha <sahajoydeep778@gmail.com>
2025-11-21 19:20:33 +0530

    Implement DFS traversal in AdjacencyList Graph

---
+++ src/graph/AdjacencyListDFS .java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+
+public class AdjacencyListDFS {
+    public static class Edge{
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int src,int dest,int wt){
+            this.src = src;
+            this.dest = dest;
+            this.wt = wt;
+        }
+    }
+
+    // to create a graph structure
+    public static void createGraph(ArrayList<Edge> graph[]){
+        for(int i=0;i<graph.length;i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        //vertex ->0
+        graph[0].add(new Edge(0, 1, 1));
+        graph[0].add(new Edge(0, 2, 1));
+        
+        //vertex ->1
+        graph[1].add(new Edge(1, 0, 1));
+        graph[1].add(new Edge(1, 3, 1));
+
+        //vertex ->2
+        graph[2].add(new Edge(2, 0, 1));
+        graph[2].add(new Edge(2, 4, 1));
+
+        //vertex ->3
+        graph[3].add(new Edge(3, 1, 1));
+        graph[3].add(new Edge(3, 4, 1));
+        graph[3].add(new Edge(3, 5, 1));
+
+        //vertex ->4
+        graph[4].add(new Edge(4, 2, 1));
+        graph[4].add(new Edge(4, 3, 1));
+        graph[4].add(new Edge(4, 5, 1));
+
+        //vertex ->5
+        graph[5].add(new Edge(5, 4, 1));
+        graph[5].add(new Edge(5, 3, 1));
+        graph[5].add(new Edge(5, 6, 1));
+
+        //vertex ->6
+        graph[6].add(new Edge(6, 5, 1));
+
+    }
+
+    // DFS traversal
+    public static void dfs(ArrayList<Edge>[] graph,int curr,boolean visit[]){ // O(V+E)
+        System.out.print(curr + " ");
+        visit[curr] = true;
+
+        for(int i=0;i<graph[curr].size();i++){
+            Edge e = graph[curr].get(i);
+            if(!visit[e.dest]){
+                dfs(graph,e.dest,visit);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int V = 7;
+        @SuppressWarnings("unchecked")
+        ArrayList<Edge>[] graph = new ArrayList[V];
+        // to create a graph to traverse
+        createGraph(graph);
+        // set visit for all vertices to false at start
+        boolean visit[] = new boolean[V];
+        for(int i=0;i<V;i++){
+            visit[i] = false;
+        }
+        dfs(graph,0,visit);     
+    }
+}
